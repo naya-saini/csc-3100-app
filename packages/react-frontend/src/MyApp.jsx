a8c9fadd396a172b4c3d27d3afa96a175ec21c18
Inayat Saini <inayatsaini25@gmail.com>
2026-08-28 11:41:37 -0700

    .

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,11 +1,29 @@
 // src/MyApp.jsx
 import React from "react";
+import Table from "./Table";
+const characters = [
+  {
+    name: "Charlie",
+    job: "Janitor",
+  },
+  {
+    name: "Mac",
+    job: "Bouncer",
+  },
+  {
+    name: "Dee",
+    job: "Aspring actress",
+  },
+  {
+    name: "Dennis",
+    job: "Bartender",
+  },
+];
 
 function MyApp() {
   return (
-    <div>
-      <h1>Hello, React!</h1>
+    <div className="container">
+      <Table characterData={characters} />
     </div>
   );
 }
-export default MyApp;
