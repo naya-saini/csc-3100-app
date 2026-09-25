27cacd0bb3dae168c16e6561db425b0b78726b9d
Inayat Saini <inayatsaini25@gmail.com>
2026-09-24 23:07:02 -0700

    edits

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -18,7 +18,7 @@ function MyApp() {
    function removeOneCharacter(index) {
   const character = characters[index];
 
-  fetch(`http://localhost:8000/users/${character.id}`, {
+  fetch(`http://localhost:8000/users/${character._id}`, {
     method: "DELETE",
   })
     .then((response) => {
