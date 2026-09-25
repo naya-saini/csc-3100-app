995e366ab4e69f835294ee0d04c474e11cc61528
Inayat Saini <inayatsaini25@gmail.com>
2026-09-24 22:47:52 -0700

    edits

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,7 +1,19 @@
 import express from "express";
 import cors from "cors";
 import userServices from "./services/user-services.js";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 
+
+dotenv.config();
+
+const { MONGO_CONNECTION_STRING } = process.env;
+
+mongoose.set("debug", true);
+mongoose
+  .connect(MONGO_CONNECTION_STRING + "users") // connect to Db "users"
+  .catch((error) => console.log(error));
+  
 const app = express();
 const port = 8000;
 
